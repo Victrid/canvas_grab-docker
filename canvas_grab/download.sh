4c0887a9ce84dc558a6654f61a497d5c77cc4be6
Victrid <weihau.chiang@gmail.com>
2021-09-26 12:42:49 +0800

    Update

--- canvas_grab/download.sh
+++ canvas_grab/download.sh
@@ -3,7 +3,7 @@ echo "==> canvas_grab starts."
 for filename in /config/*.conf; do
     [ -e "$filename" ] || continue
     echo " -> Loading $filename ..."
-    /canvas/main.py -Ikc $filename
+    python /canvas/main.py -Ikc $filename
     echo " -> $filename finished."
 done 
 echo "==> canvas_grab stops."
--- canvas_grabd/schedule.sh
+++ canvas_grabd/schedule.sh
@@ -1,4 +1,24 @@
-#/bin/bash
-echo "$SCHEDULE /canvas/download.sh 2>&1" > /etc/cron.d/canvas
+#!/bin/bash
+STDOUT=/proc/1/fd/1
+STDERR=/proc/1/fd/2
+echo " ┌────────────────────────────────┐" > $STDOUT 
+echo " │     Canvas grabd  v2.2.0       │" > $STDOUT 
+echo " │                                │" > $STDOUT 
+echo " │      Visit GitHub repo:        │" > $STDOUT 
+echo " │  github.com/skyzh/canvas_grab  │" > $STDOUT 
+echo " │       for instructions         │" > $STDOUT 
+echo " │                                │" > $STDOUT 
+echo " │  This image is not official    │" > $STDOUT 
+echo " │  Visit Docker hub for usage    │" > $STDOUT 
+echo " │     victrid/canvas_grabd       │" > $STDOUT 
+echo " └────────────────────────────────┘" > $STDOUT 
+echo "Exporting env variables..." > $STDOUT
+printenv | sed 's/^\(.*\)$/export \1/g' > /etc/profile
+echo "Scheduler settings: $SCHEDULE" > $STDOUT
+echo "$SCHEDULE /bin/sh -c 'source /etc/profile && /canvas/download.sh'" > $STDOUT 2>$STDERR" > /etc/cron.d/canvas
+echo "Applying crontab..." > $STDOUT
 crontab /etc/cron.d/canvas
+echo "Current crontab:" > $STDOUT
+crontab -l > $STDOUT 2>$STDERR
+cron -f > $STDOUT 2>$STDERR
 
